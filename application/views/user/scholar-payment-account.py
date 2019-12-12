4b8496f0312f8e6d14412f57d0185c992cf316c3
Celery Qin <celerysoft@gmail.com>
2019-12-12 17:21:48 +0800

    update: ScholarPaymentAccountAPI

--- application/views/user/scholar-payment-account.py
+++ application/views/user/scholar-payment-account.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+from decimal import Decimal
+
 from application import exception
 from application.model.scholar_payment_account import ScholarPaymentAccount
+from application.util import scholar_payment_system
 from application.util.database import session_scope
 from application.views.base_api import BaseNeedLoginAPI, ApiResult
 
 
-class ScholarBalanceAPI(BaseNeedLoginAPI):
+class ScholarPaymentAccountAPI(BaseNeedLoginAPI):
     methods = ['GET']
 
     def get(self):
@@ -23,39 +26,24 @@ class ScholarBalanceAPI(BaseNeedLoginAPI):
             })
             return result.to_response()
 
-    # def patch(self):
-    #     """
-    #     api_update_scholar_balance
-    #     :return:
-    #     """
-    #     target_user_id = self.get_post_data('user_id', require=True, error_message='缺少user_id字段')
-    #     amount = self.get_post_data('amount', require=True, error_message='缺少amount字段')
-    #     try:
-    #         amount = int(amount)
-    #         if amount <= 0:
-    #             raise exception.api.InvalidRequest('金额必须大于0')
-    #     except BaseException as e:
-    #         raise exception.api.InvalidRequest('请输入合法的金额：{}'.format(e))
-    #
-    #     with session_scope() as session:
-    #         if not permission.toolkit.check_manage_scholar_balance_permission(session, self.user_id):
-    #             raise exception.api.Forbidden('当前用户无权管理学术积分')
-    #
-    #         user_scholar_balance = session.query(UserScholarBalance) \
-    #             .filter(UserScholarBalance.user_id == target_user_id).first()
-    #
-    #         user_scholar_balance.balance += amount
-    #
-    #         user_scholar_balance_log = UserScholarBalanceLog(
-    #             user_id=target_user_id,
-    #             amount=amount,
-    #             balance=user_scholar_balance.balance,
-    #             message='系统发放'
-    #         )
-    #         session.add(user_scholar_balance_log)
-    #
-    #     result = ApiResult('充值学术积分成功', 201)
-    #     return make_response(result.to_response())
+    def patch(self):
+        target_user_uuid = self.get_post_data('user_uuid', require=True, error_message='缺少user_uuid字段')
+        amount = self.get_post_data('amount', require=True, error_message='缺少amount字段')
+        try:
+            amount = Decimal(amount)
+            if amount <= 0:
+                raise exception.api.InvalidRequest('充值金额必须大于0')
+        except BaseException as e:
+            raise exception.api.InvalidRequest('请输入合法的金额：{}'.format(e))
+
+        # TODO check permission
+        # with session_scope() as session:
+        #     if not permission.toolkit.check_manage_scholar_balance_permission(session, self.user_id):
+        #         raise exception.api.Forbidden('当前用户无权管理学术积分')
+        scholar_payment_system.toolkit.recharge(target_user_uuid, amount)
+
+        result = ApiResult('学术积分充值的请求已收到，正在后台充值', 200)
+        return result.to_response()
 
 
-view = ScholarBalanceAPI
+view = ScholarPaymentAccountAPI
